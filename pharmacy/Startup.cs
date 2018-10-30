fdbbffd7acef9441110554532ad3fd2f8fb0b9f4
muhamed ibrahim <mohamed77ebrahim@yahoo.com>
2018-10-30 13:24:54 +0200

    Add project files.

---
+++ pharmacy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pharmacy.Startup))]
+namespace pharmacy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
